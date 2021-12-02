2843f6a8d256166519e3bca55b7cdda83b9c95cb
PubupCreed <69687087+PubupCreed@users.noreply.github.com>
2021-12-03 00:09:44 +0200

    update js

--- lesson8/js/main.js
+++ lesson8/js/main.js
@@ -1,11 +1,12 @@
 class Worker {
-
-    firstName = 'Ivan';
-    lastName = 'Chaplinskyi';
-    rate = 1000;
-    hours = 160;
-    overtime = 170;
-    
+    constructor(firstName, lastName, rate, hours, overtime) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.rate = rate;
+        this.hours = hours;
+        this.overtime = overtime;
+    }
+ 
     get fullName() {
         return `${this.firstName} ${this.lastName}`;
     };
@@ -16,13 +17,33 @@ class Worker {
 };
 
 class SoftwareEngineer extends Worker {
+    constructor(
+        position,
+        role,
+        skills,
+        firstName, 
+        lastName, 
+        rate, 
+        hours, 
+        overtime
+    ) {
+       super(firstName, lastName, rate, hours, overtime)
+       this.position = position;
+       this.role = role;
+       this.skills = skills;
+    }
 
-    position = 'Junior';
-    role = 'Fullstack';
-    skills = ['React JS', 'Node JS'];
+    static list = {
+        Junior: 1.2,
+        Middle: 1.5,
+        Senior: 1.8,
+        Backend: 0.2,
+        Frontend: 0.3,
+        Fullstack: 0.4
+    }
 
     get bonusCoefficient() {
-       return 1.2 + 0.4;
+        SoftwareEngineer.list[this.position] + SoftwareEngineer.list[this.role]
     };
 
     get bonus() {
@@ -33,12 +54,25 @@ class SoftwareEngineer extends Worker {
         return this.salary + this.bonus;
     };
 
-    static getFullInfo() {
-       const job = new SoftwareEngineer(this.fullName, this.bonusCoefficient, this.bonus, this.fullSalary);
-       console.log(job)
+    getFullInfo() {
+        return Object.keys(this).reduce((obj, key) =>{
+            obj[key] = this[key];
+            return obj;
+        }, {})
     };
 };
 
-/* SoftwareEngineer.getFullInfo(); */
+const developer = new SoftwareEngineer(
+    'Junior',
+    'Fullstack',
+    ['React JS', 'Node JS'],
+    'Ivan',
+    'Chaplinskyi',
+    3,
+    170,
+    30
+)
 
-console.log(new SoftwareEngineer)
+console.log(developer.fullName)
+console.log(developer.salary)
+console.log(developer.getFullInfo())
